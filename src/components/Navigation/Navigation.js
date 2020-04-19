f89c15148938fc85f2c039d2fa6703c9f07e6cc8
Edwin Agbenyega <eddybanks03@gmail.com>
2020-04-18 19:25:05 -0600

    Add logo to navigation

--- src/components/Navigation/Navigation.js
+++ src/components/Navigation/Navigation.js
@@ -1,12 +1,18 @@
 import React from 'react'
 import styles from '../../stylesheets/Navigation.module.css'
 import { Link } from 'react-router-dom'
-import { Nav } from 'react-bootstrap'
+import { Nav, NavbarBrand } from 'react-bootstrap'
+import Logo from '../../images/Noble Christian School Logo_01.png'
 
 const Navigation = (props) => (
-  <Nav defaultActiveKey="/" className="flex-column">
+  <Nav variant='pills' defaultActiveKey="/" className="flex-column">
+    <Nav.Item>
+      <Nav.Link as={Link} to={props.navBrand.path} key={props.navBrand.id}>
+        <img src={Logo} className={styles.logo} alt={props.navBrand.title} />
+      </Nav.Link>
+    </Nav.Item>
     {props.navItems.map(navItem => (
-      <Nav.Item>
+      <Nav.Item className={styles.navItems}>
         <Nav.Link as={Link} to={navItem.path} key={navItem.id}>{navItem.title}</Nav.Link>
       </Nav.Item>
     ))}
--- src/components/Navigation/NavigationContainer.js
+++ src/components/Navigation/NavigationContainer.js
@@ -5,8 +5,8 @@ class NavigationContainer extends Component {
   constructor(props) {
     super(props)
     this.state = {
+      navBrand: { id: 0, title: 'Noble Christian School', path: '/'},
       navItems: [
-        { id: 0, title: 'Noble Christian School', path: '/'},
         { id: 1, title: 'Student Applications', path: '/applications'},
         { id: 2, title: 'School Fees', path: '/tuition'},
         { id: 3, title: 'Help', path: '/help'}
@@ -15,10 +15,10 @@ class NavigationContainer extends Component {
   }
 
   render() {
-    const { navItems } = this.state
+    const { navItems, navBrand } = this.state
     return (
       <Fragment>
-        <Navigation navItems={navItems} />
+        <Navigation navItems={navItems} navBrand={navBrand} />
       </Fragment>
     )
   }

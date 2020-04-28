21ea469133238450b0a6aeedb4ecf0c611f25e5c
Carl <carl.agbenyega@gmail.com>
2020-04-28 17:28:03 -0600

    created summaryStats custom component, updated landing page

--- src/components/Landing/LandingContainer.js
+++ src/components/Landing/LandingContainer.js
@@ -10,7 +10,7 @@ class LandingContainer extends Component {
       summaryStats: [
         {id: '0', title: 'Current Number of Students', value: 118},
         {id: '1', title: 'Average Number of Students per Class', value: 16},
-        {id: '2', title: 'Total Amount of School Fees Owed', value: 'GHC 7000'},
+        {id: '2', title: 'Total Amount of School Fees Owed (GHC)', value: 7000},
         {id: '3', title: 'Number of Students in Arrears', value: 30}
       ]
     }    

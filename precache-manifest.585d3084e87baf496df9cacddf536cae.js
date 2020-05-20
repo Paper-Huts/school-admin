489f7124ec1a52df5ec553d20b21e7a8ccf23db1
Edwin Agbenyega <eddybanks03@gmail.com>
2020-05-19 23:29:28 -0600

    Updates

--- precache-manifest.585d3084e87baf496df9cacddf536cae.js
+++ precache-manifest.585d3084e87baf496df9cacddf536cae.js
@@ -1,27 +1,27 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "5b8440007f62ddff008bf5f122ff0961",
+    "revision": "2bac7b84b7d044ca4764dedd538011ff",
     "url": "/noble-dbms-app/index.html"
   },
   {
-    "revision": "37864d3176867a051686",
+    "revision": "dd746dcf5a39837fdd70",
     "url": "/noble-dbms-app/static/css/2.de424728.chunk.css"
   },
   {
-    "revision": "da6a8d9dfe8c740ed90e",
-    "url": "/noble-dbms-app/static/css/main.728fa588.chunk.css"
+    "revision": "e3b95029d33754a7f9bd",
+    "url": "/noble-dbms-app/static/css/main.abeb9a3d.chunk.css"
   },
   {
-    "revision": "37864d3176867a051686",
-    "url": "/noble-dbms-app/static/js/2.ae1ddbaa.chunk.js"
+    "revision": "dd746dcf5a39837fdd70",
+    "url": "/noble-dbms-app/static/js/2.10cf5f34.chunk.js"
   },
   {
     "revision": "996e97a9c59b2ad74d9e36e982ab30fe",
-    "url": "/noble-dbms-app/static/js/2.ae1ddbaa.chunk.js.LICENSE.txt"
+    "url": "/noble-dbms-app/static/js/2.10cf5f34.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "da6a8d9dfe8c740ed90e",
-    "url": "/noble-dbms-app/static/js/main.579640f5.chunk.js"
+    "revision": "e3b95029d33754a7f9bd",
+    "url": "/noble-dbms-app/static/js/main.f567c8f0.chunk.js"
   },
   {
     "revision": "be1534b95f3003906c23",
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/noble-dbms-app/precache-manifest.83a7752b8b5a814cad3411ac32aa9335.js"
+  "/noble-dbms-app/precache-manifest.585d3084e87baf496df9cacddf536cae.js"
 );
 
 self.addEventListener('message', (event) => {

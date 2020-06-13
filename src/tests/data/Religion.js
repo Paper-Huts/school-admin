98e4cf5b8dc11a65d0cd22e4ff3824f747e65c1f
Edwin Agbenyega <eddybanks03@gmail.com>
2020-06-13 12:40:01 -0600

    Some minor mistake correction

---
+++ src/tests/data/Religion.js
@@ -0,0 +1,8 @@
+const religions = [
+  { id: '123q33rfwef', name: 'Christian' },
+  { id: '123erqw12ef', name: 'Muslim' },
+  { id: '123q33rfw1f', name: 'Hindu' },
+  { id: '123q3vfsfgf', name: 'Buddhist' },
+  { id: '123qvsfdh4f', name: 'No Religion/Atheist/Agnostic' },
+  { id: '123q3beytjf', name: 'Other' }
+]

5dd2c8a99b74ef76d6f466301d26b77707cba1cb
Edwin Agbenyega <eddybanks03@gmail.com>
2020-06-13 11:43:51 -0600

    Need to figure out why authentication isn't working

--- src/components/AuthPages/SignIn.js
+++ src/components/AuthPages/SignIn.js
@@ -1,30 +1,77 @@
-import React from 'react'
+import React, { Component } from 'react'
 import { Form, Button, ButtonGroup } from 'react-bootstrap'
 
-import { signInWithGoogle } from '../../firebase/firebase.utils'
+import { auth, signInWithGoogle } from '../../firebase/firebase.utils'
 import styles from '../../stylesheets/AuthPages.module.css'
 
 import MiniHeader from '../CustomComponents/MiniHeader'
 
-const SignIn = () => (
-  <div className={styles.signIn}>
-    <MiniHeader title='Login' />
-    <Form>
-      <Form.Group>
-        <Form.Label>E-mail address</Form.Label>
-        <Form.Control type='email' placeholder='Enter email' />
-      </Form.Group>
+class SignIn extends Component {
+  constructor(props) {
+    super(props)
 
-      <Form.Group controlId='signInPassword'>
-        <Form.Label>Password</Form.Label>
-        <Form.Control type='password' placeholder='Password' />
-      </Form.Group>
-    </Form>
-    <ButtonGroup>
-      <Button variant='secondary'>Login</Button>
-      <Button onClick={() => signInWithGoogle()}>Login with Google</Button>
-    </ButtonGroup>
-  </div>
-)
+    this.state = {
+      email: '',
+      password: ''
+    }
+  }
+
+  handleSubmit = async event => {
+    event.preventDefault();
+
+    const { email, password } = this.state
+
+    try {
+      await auth.signInWithEmailAndPassword(email, password)
+      this.setState({ email: '', password: ''})
+    } catch (error) {
+      console.error(error)
+    }
+    
+    this.setState({ email: '', password: '' })
+  }
+
+  handleChange = event => {
+    const { value, name } = event.target
+
+    this.setState({ [name]: value })
+  }
+
+  render() {
+    const { email, password } = this.state
+    return (
+      <div className={styles.signIn}>
+        <MiniHeader title='Login' />
+        <Form onSubmit={this.handleSubmit}>
+          <Form.Group>
+            <Form.Label>E-mail address</Form.Label>
+            <Form.Control 
+              controlid='signInEmail'
+              name='email'
+              type='email' 
+              placeholder='Enter email' 
+              onChange={this.handleChange} 
+              value={email} />
+          </Form.Group>
+
+          <Form.Group controlId='signInPassword'>
+            <Form.Label>Password</Form.Label>
+            <Form.Control
+              controlid='signInPassword' 
+              name='password'
+              type='password' 
+              placeholder='Password' 
+              onChange={this.handleChange} 
+              value={password} />
+          </Form.Group>
+        </Form>
+        <ButtonGroup>
+          <Button variant='secondary' type='submit'>Login</Button>
+          <Button onClick={signInWithGoogle}>Login with Google</Button>
+        </ButtonGroup>
+      </div>
+    )
+  }
+}
 
 export default SignIn
--- src/components/AuthPages/SignUp.js
+++ src/components/AuthPages/SignUp.js
@@ -1,32 +1,113 @@
-import React from 'react'
+import React, { Component } from 'react'
 import { Form, Button } from 'react-bootstrap'
 import PropTypes from 'prop-types'
 
+import { auth, createUserProfileDocument } from '../../firebase/firebase.utils'
+
 import MiniHeader from '../CustomComponents/MiniHeader'
 
-const SignUp = () => (
-  <div className='signUpPage'>    
-    <MiniHeader title='Sign Up' />
-    <Form>
-      <Form.Group>
-        <Form.Label>E-mail address</Form.Label>
-        <Form.Control type='email' placeholder='Enter email' />
-      </Form.Group>
+class SignUp extends Component {
+  constructor() {
+    super()
 
-      <Form.Group controlId='signInPassword'>
-        <Form.Label>Password</Form.Label>
-        <Form.Control type='password' placeholder='Password' />
-      </Form.Group>
+    this.state = {
+      displayName: '',
+      email: '',
+      password: '',
+      confirmPassword: ''
+    }
+  }
 
-      <Form.Group controlId='signInPassword'>
-        <Form.Label>Confirm Password</Form.Label>
-        <Form.Control type='password' placeholder='Confirm Password' />
-      </Form.Group>
-    </Form>
+  handleSubmit = async event => {
+    event.preventDefault()
 
-    <Button variant='secondary'>Sign Up</Button>
-  </div>
-)
+    const {displayName, email, password, confirmPassword} = this.state
+
+    if(password !== confirmPassword) {
+      alert("Passwords don't match")
+
+      return
+    }
+
+    try {
+      const { user } = await auth.createUserWithEmailAndPassword(email, password)
+
+      await createUserProfileDocument(user, { displayName })
+
+      this.setState({
+        displayName: '',
+        email: '',
+        password: '',
+        confirmPassword: ''
+      })
+    } catch(error) {
+      console.error(error)
+    }
+  }
+
+  handleChange = event => {
+    const { name, value } = event.target
+
+    this.setState({ [name]: value })
+  }
+
+  render() {
+    const {displayName, email, password, confirmPassword} = this.state
+
+    return (
+      <div className='signUpPage'>    
+        <MiniHeader title='Sign Up' />
+        <Form onSubmit={this.handleSubmit}>
+          <Form.Group>
+            <Form.Label>Full Name</Form.Label>
+            <Form.Control 
+              controlid='signUpName'
+              name='displayName'
+              type='text' 
+              placeholder='Full Name' 
+              onChange={this.handleChange} 
+              value={displayName} />
+          </Form.Group>
+
+          <Form.Group>
+            <Form.Label>E-mail address</Form.Label>
+            <Form.Control 
+              controlid='signUpEmail'
+              name='email'
+              type='email' 
+              placeholder='Enter email' 
+              onChange={this.handleChange} 
+              value={email} />
+          </Form.Group>
+
+          <Form.Group controlId='signInPassword'>
+            <Form.Label>Password</Form.Label>
+            <Form.Control 
+              controlid='signUpPassword'
+              name='password'
+              type='password' 
+              placeholder='Password' 
+              onChange={this.handleChange} 
+              value={password} />
+          </Form.Group>
+
+          <Form.Group controlId='signInPassword'>
+            <Form.Label>Confirm Password</Form.Label>
+            <Form.Control
+              controlid='signUpConfirmPassword' 
+              name='confirmPassword'
+              type='password' 
+              placeholder='Confirm Password' 
+              onChange={this.handleChange} 
+              value={confirmPassword} />
+          </Form.Group>
+        </Form>
+
+        <Button variant='secondary' type='submit'>Sign Up</Button>
+      </div>
+    )
+  }
+}
 
 SignUp.propTypes = {
   email: PropTypes.string,

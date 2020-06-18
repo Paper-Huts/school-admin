ec958d2cd4bc0b314761cf9f1db3015776172200
Carl <carl.agbenyega@gmail.com>
2020-06-17 20:46:31 -0600

    created tuition selectors for tuition payment records/history and list of tuition by grade

--- src/redux/Tuition/TuitionSelectors.js
+++ src/redux/Tuition/TuitionSelectors.js
@@ -2,7 +2,23 @@ import { createSelector } from 'reselect';
 
 const selectTuition = state => state.tuition;
 
-export const selectTuitionByGradeLevel = createSelector(
+export const selectTuitionByGrade = createSelector(
   [selectTuition],
-  tuition => tuition.tuitionByGradeLevel
+  tuition => tuition.tuitionByGrade
+);
+
+export const selectTuitionByGradeForList = createSelector(
+  [selectTuitionByGrade],
+  tuitionByGrade => Object.keys(tuitionByGrade).map(grade => tuitionByGrade[grade])
+);
+
+export const selectTuitionPaymentHistory = createSelector(
+  [selectTuition],
+  tuition => tuition.tuitionPaymentHistory
+);
+
+//returns the list of payment history records given a particular student record
+export const selectStudentTuitionPaymentHistory = studentUid => createSelector(
+  [selectTuitionPaymentHistory],
+  tuitionPaymentHistory => tuitionPaymentHistory.filter(record => record.studentUid === studentUid)
 );

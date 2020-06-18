958f5e2c3f22f65ce0b4af0cb43f6446cbbb6024
Carl <carl.agbenyega@gmail.com>
2020-06-17 20:47:08 -0600

    updated school period reducer and selectors

--- src/redux/SchoolPeriod/SchoolPeriodSelectors.js
+++ src/redux/SchoolPeriod/SchoolPeriodSelectors.js
@@ -6,3 +6,13 @@ export const selectCurrentSchoolPeriod = createSelector(
   [selectSchoolPeriod],
   schoolPeriod => schoolPeriod.currentSchoolPeriod
 );
+
+export const selectSchoolPeriods = createSelector(
+  [selectSchoolPeriod],
+  schoolPeriod => schoolPeriod.schoolPeriods
+);
+
+export const selectSchoolPeriodByYearAndTerm = (year, term) => createSelector(
+  [selectSchoolPeriods],
+  schoolPeriods => schoolPeriods.find(schoolPeriod => schoolPeriod.academicYear === year && schoolPeriod.academicTerm === term)
+);

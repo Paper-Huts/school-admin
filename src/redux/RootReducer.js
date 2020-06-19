fead9ed299546d6f997790491f9575b5eb63da1b
Carl <carl.agbenyega@gmail.com>
2020-06-18 22:48:51 -0600

    updated rootreducer - whitelisted tuition state for persistence

--- src/redux/RootReducer.js
+++ src/redux/RootReducer.js
@@ -12,7 +12,7 @@ import studentsReducer from './Students/StudentsReducer'
 const persistConfig = {
   key: 'root',
   storage,
-  whitelist: ['navigation', 'schoolStats']
+  whitelist: ['navigation', 'schoolStats', 'tuition']
 }
 
 const rootReducer = combineReducers({

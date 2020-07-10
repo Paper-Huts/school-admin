602c3d0f8864ff65e32ddc0d30c4d297852554b6
Carl <carl.agbenyega@gmail.com>
2020-07-10 00:14:00 -0600

    Update GradeTuition.js

--- src/components/Tuition/GradeTuition/GradeTuition.js
+++ src/components/Tuition/GradeTuition/GradeTuition.js
@@ -5,36 +5,45 @@ import { createStructuredSelector } from "reselect";
 import { Container, Row, Col } from "react-bootstrap";
 import Header from "../../CustomComponents/Header";
 import CurrentSchoolPeriodBar from "../../CustomComponents/CurrentSchoolPeriodBar";
+import AddTuitionForm from "./AddGradeTuitionForm";
 
 import { selectTuitionByGradeForList } from "../../../redux/Tuition/TuitionSelectors";
 
 import styles from "../../../stylesheets/CustomComponents.module.css";
 
-const GradeTuition = ({tuitionByGradeLevel}) => {
-  return(
+const GradeTuition = ({ tuitionByGradeLevel }) => {
+  return (
     <Container fluid>
       <Header header="Tuition Update" />
       <CurrentSchoolPeriodBar />
-        <Row className={styles.subSection}>
-          {tuitionByGradeLevel.map((tuition) => (
-            <Col key={tuition.id} md={6} xs={12}>
-              <h4>{tuition.gradeLevel}</h4>
-              <ul>
-                <li>Tuition - GHC {tuition.costOfTuition}</li>
-                <li>
-                  Book & Stationery - GHC {tuition.costOfBooksAndStationery}
-                </li>
-                <li>Misc Items - GHC {tuition.costOfMiscItems}</li>
-              </ul>
-            </Col>
-          ))}
-        </Row>
+      <Row>
+        <Col md={6} xs={12}>
+          <AddTuitionForm />
+        </Col>
+        <Col md={6} xs={12}>
+          <h3>Tuition for Current School Year</h3>
+          <Row className={styles.subSection}>
+            {tuitionByGradeLevel.map((tuition) => (
+              <Col key={tuition.id} md={6} xs={12}>
+                <h5>{tuition.name}</h5>
+                <ul>
+                  <li>Tuition - GHC {tuition.costOfTuition}</li>
+                  <li>
+                    Book & Stationery - GHC {tuition.costOfBooksAndStationery}
+                  </li>
+                  <li>Misc Items - GHC {tuition.costOfMiscItems}</li>
+                </ul>
+              </Col>
+            ))}
+          </Row>
+        </Col>
+      </Row>
     </Container>
-  )
-}
+  );
+};
 
 const mapStateToProps = createStructuredSelector({
-  tuitionByGradeLevel: selectTuitionByGradeForList
+  tuitionByGradeLevel: selectTuitionByGradeForList,
 });
 
 export default connect(mapStateToProps)(GradeTuition);

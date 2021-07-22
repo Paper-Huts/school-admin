a6bb31c4277fe5c07530f2756d0619ff14d02746
Edwin Agbenyega <eddybanks03@gmail.com>
2021-07-22 00:42:19 -0700

    Correct error with update student info

--- src/App.js
+++ src/App.js
@@ -1,23 +1,23 @@
-import React, { Fragment, Component } from 'react'
-import { Container } from 'react-bootstrap'
-import { Switch, Route, Redirect } from 'react-router-dom'
-import { connect } from 'react-redux'
-import { createStructuredSelector } from 'reselect'
+import React, { Fragment, Component } from "react";
+import { Container } from "react-bootstrap";
+import { Switch, Route, Redirect } from "react-router-dom";
+import { connect } from "react-redux";
+import { createStructuredSelector } from "reselect";
 
 import { auth, createUserProfileDocument } from "./firebase/firebase.utils";
 import { setCurrentUser } from "./redux/User/UserActions";
 import { selectCurrentUser } from "./redux/User/UserSelectors";
 
-import NavigationContainer from './components/Navigation/NavigationContainer'
-import LandingContainer from './components/Landing/LandingContainer'
-import AdmissionsContainer from './components/Admissions/AdmissionsContainer'
-import TuitionContainer from './components/Tuition/TuitionContainer'
-import Help from './components/Help/Help'
-import TeachersContainer from './components/Teachers/TeachersContainer'
-import NewStudent from './components/Admissions/NewStudent/NewStudent'
-import UpdateStudent from './components/Admissions/UpdateStudent/UpdateStudent'
-import AuthPages from './components/AuthPages/AuthPages'
-import UserContainer from './components/User/UserContainer'
+import NavigationContainer from "./components/Navigation/NavigationContainer";
+import LandingContainer from "./components/Landing/LandingContainer";
+import AdmissionsContainer from "./components/Admissions/AdmissionsContainer";
+import TuitionContainer from "./components/Tuition/TuitionContainer";
+import Help from "./components/Help/Help";
+import TeachersContainer from "./components/Teachers/TeachersContainer";
+import NewStudent from "./components/Admissions/NewStudent/NewStudent";
+import UpdateStudent from "./components/Admissions/UpdateStudent/UpdateStudent";
+import AuthPages from "./components/AuthPages/AuthPages";
+import UserContainer from "./components/User/UserContainer";
 
 class App extends Component {
   unsubscribeFromAuth = null;
@@ -54,15 +54,31 @@ class App extends Component {
         <NavigationContainer />
         <Container fluid>
           <Switch>
-              <Route exact path='/' component={LandingContainer} />
-              <Route exact path='/admissions' component={AdmissionsContainer} />
-              <Route path='/tuition' component={TuitionContainer} />
-              <Route exact path='/help' component={Help} />
-              <Route exact path='/teachers' component={TeachersContainer} />
-              <Route exact path='/admissions/new_student' component={NewStudent} />
-              <Route exact path='/admissions/update_student' component={UpdateStudent} />
-              <Route exact path='/account' render={() => currentUser ? (<UserContainer />) : (<AuthPages />)} />
-              <Route exact path='/login' render={() => currentUser ? (<Redirect to='/' />) : (<AuthPages />)} />
+            <Route exact path="/" component={LandingContainer} />
+            <Route exact path="/admissions" component={AdmissionsContainer} />
+            <Route path="/tuition" component={TuitionContainer} />
+            <Route exact path="/help" component={Help} />
+            <Route exact path="/teachers" component={TeachersContainer} />
+            <Route
+              exact
+              path="/admissions/new_student"
+              component={NewStudent}
+            />
+            <Route
+              exact
+              path="/admissions/update_student_info"
+              component={UpdateStudent}
+            />
+            <Route
+              exact
+              path="/account"
+              render={() => (currentUser ? <UserContainer /> : <AuthPages />)}
+            />
+            <Route
+              exact
+              path="/login"
+              render={() => (currentUser ? <Redirect to="/" /> : <AuthPages />)}
+            />
           </Switch>
         </Container>
       </Fragment>
--- src/redux/Navigation/NavigationReducer.js
+++ src/redux/Navigation/NavigationReducer.js
@@ -1,30 +1,38 @@
-import NavigationActionTypes from './NavigationActionTypes'
+import NavigationActionTypes from "./NavigationActionTypes";
 
 const INITIAL_STATE = {
-  navBrand: { id: 0, title: 'School Admin', path: '/'},
+  navBrand: { id: 0, title: "School Admin", path: "/" },
   navItems: [
-    { id: 1, title: 'Admissions', path: '/admissions'},
-    { id: 2, title: 'Tuition', path: '/tuition'},
-    { id: 3, title: 'Teachers', path: '/teachers'}
+    { id: 1, title: "Admissions", path: "/admissions" },
+    { id: 2, title: "Tuition", path: "/tuition" },
+    { id: 3, title: "Teachers", path: "/teachers" },
   ],
   admissionsNav: {
-    header: 'Admissions',
+    header: "Admissions",
     options: [
-      { id: 1, title: 'New Student Application', path: '/admissions/new_student' },
-      { id: 2, title: 'Update Student Info', path: 'admissions/update_student_info'}
-    ]
-  }
-}
+      {
+        id: 1,
+        title: "New Student Application",
+        path: "/admissions/new_student",
+      },
+      {
+        id: 2,
+        title: "Update Student Info",
+        path: "admissions/update_student_info",
+      },
+    ],
+  },
+};
 
-const navigationReducer = (state=INITIAL_STATE, action) => {
-  switch(action.type) {
+const navigationReducer = (state = INITIAL_STATE, action) => {
+  switch (action.type) {
     case NavigationActionTypes.MAIN_NAVIGATION:
-      return { ...state.navBrand, ...state.navItems }
+      return { ...state.navBrand, ...state.navItems };
     case NavigationActionTypes.ADMISSIONS_OPTIONS:
-      return state.admissionsNav
-    default: 
-      return state
+      return state.admissionsNav;
+    default:
+      return state;
   }
-}
+};
 
-export default navigationReducer
+export default navigationReducer;
